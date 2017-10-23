589fc1de371d327a0943de428f2f22d4158d7829
wkuo <wuichia@gmail.com>
2017-10-24 07:34:33 +0800

    Make Ajax call

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,11 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/wuichia.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+    }
+  });
 
 });
