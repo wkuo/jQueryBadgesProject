a700b369fcb116f2da6b236fe1ca0b1b430de6df
wkuo <wuichia@gmail.com>
2017-10-24 08:10:48 +0800

    Adding Course Buttons

--- src/assets/main.js
+++ src/assets/main.js
@@ -5,10 +5,12 @@ $(function() {
     dataType: 'jsonp',
     success: function(response) {
       for (i = 0; i < response.courses.completed.length; i++) {
+        course = response.courses.completed[i];
         $("#badges").append("<div></div>");
         $("#badges div").addClass("course");
-        $(".course").append("<h3>"+ response.courses.completed[i].title +"</h3>");
-        $(".course").append("<img src='"+ response.courses.completed[i].badge +"'></img>");
+        $(".course").append("<h3>" + course.title + "</h3>");
+        $(".course").append("<img src='" + course.badge + "'></img>");
+        $(".course").append("<a target='_blank' href='" + course.url + "' class='btn btn-primary'>See Course</a> ");
       }
     }
   });
